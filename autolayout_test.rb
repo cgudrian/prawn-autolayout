36c5c00852f237f69cd4d941912625449e69b909
Christian Gudrian <christian@gudrian.org>
2014-05-20 22:37:13 +0200

    fixed test

--- autolayout_test.rb
+++ autolayout_test.rb
@@ -5,12 +5,21 @@ require_relative 'lib/prawn-autolayout'
 require 'prawn/measurement_extensions'
 
 Prawn::Document.generate('boxtest.pdf') do
+
+  line_width 0.1.mm
+
   auto_layout do |layout|
 
     stroke_axis
 
     layout.frame do
       stroke_bounds
+      layout.frame do
+        stroke_bounds
+      end
+      layout.frame do
+        stroke_bounds
+      end
     end
 
     layout.frame do
@@ -21,6 +30,12 @@ Prawn::Document.generate('boxtest.pdf') do
       stroke_bounds
       layout.frame do
         stroke_bounds
+        layout.frame do
+          stroke_bounds
+        end
+        layout.frame do
+          stroke_bounds
+        end
       end
       layout.frame do
         stroke_bounds
@@ -44,7 +59,6 @@ Prawn::Document.generate('boxtest.pdf') do
         end
       end
     end
-
   end
 end
 
