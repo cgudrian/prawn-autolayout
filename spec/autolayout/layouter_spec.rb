48d821fc41c2d219936f768ab24b6443631a444f
Christian Gudrian <christian@gudrian.org>
2014-05-23 08:53:06 +0200

    refactored spec file layout

---
+++ spec/autolayout/layouter_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+module Prawn
+  module AutoLayout
+
+    describe Layouter do
+      it 'should layout two frames evenly across the available width' do
+        document = mock_document(0, 100, 100, 100)
+
+        document.should_receive(:bounding_box).with([0, 100], { width: 100, height: 100 }).and_yield
+        document.should_receive(:bounding_box).with([0, 100], { width: 50, height: 100 }).and_yield
+        document.should_receive(:bounding_box).with([50, 100], { width: 50, height: 100 }).and_yield
+
+        Prawn::AutoLayout::Layouter.new(document) do |l|
+          l.columns do
+            l.frame do
+            end
+            l.frame do
+            end
+          end
+        end
+      end
+
+      it 'should layout nested frames evenly across the available width' do
+        document = mock_document(0, 100, 100, 100)
+
+        document.should_receive(:bounding_box).with([0, 100], { width: 100, height: 100 }).and_yield
+
+        document.should_receive(:bounding_box).with([0, 100], { width: 50, height: 100 }).and_yield
+        document.should_receive(:bounding_box).with([0, 100], { width: 25, height: 100 }).and_yield
+        document.should_receive(:bounding_box).with([25, 100], { width: 25, height: 100 }).and_yield
+
+        document.should_receive(:bounding_box).with([50, 100], { width: 50, height: 100 }).and_yield
+        document.should_receive(:bounding_box).with([0, 100], { width: 25, height: 100 }).and_yield
+        document.should_receive(:bounding_box).with([25, 100], { width: 25, height: 100 }).and_yield
+
+        Layouter.new(document) do |l|
+          l.columns do
+            l.frame do
+              l.frame do
+              end
+              l.frame do
+              end
+            end
+            l.frame do
+              l.frame do
+              end
+              l.frame do
+              end
+            end
+          end
+        end
+      end
+    end
+
+
+  end
+end
